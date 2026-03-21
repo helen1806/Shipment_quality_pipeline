92222aea15fce9aab17c5571679954e8978be3f2
helen <helenmarys1023@gmail.com>
2026-03-21 15:19:12 +0530

    added reading of csv files

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
 from flask import Flask, render_template, request
-
+import os
 app = Flask(__name__)
-
+import pandas as pd
 
 @app.route('/')
 def home():
     return render_template("index.html")
-
+    
 @app.route('/upload', methods=['POST'])
 def uploading():
     files = request.files.getlist('file')
+    file_columns = {}
 
     for file in files:
-        if  file.filename != "":
-            file.save('uploads/' + file.filename)
+        if file.filename != "":
+            path = os.path.join('uploads', file.filename)
+            file.save(path)
 
-    return "Files uploaded successfully"
+            df = pd.read_csv(path)
+            file_columns[file.filename] = df.columns.tolist()
 
+    return render_template(
+        "index.html",
+        column_section=True,
+        file_columns=file_columns
+    )
+    
+
+    
+    
+    
 if __name__ == "__main__":
     app.run(debug=True)
