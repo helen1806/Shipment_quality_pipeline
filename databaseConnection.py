f7d5fb8947c8cb7a096bd1ba3f443863d79522b7
helen <helenmarys1023@gmail.com>
2026-03-24 23:52:18 +0530

    inserting userid first

--- databaseConnection.py
+++ databaseConnection.py
@@ -1,6 +1,20 @@
+from dotenv import load_dotenv
+
 import os
 from supabase import create_client, Client
 
+load_dotenv()
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
+
+def insert_user_dataset(table_name):
+
+    table_name = os.path.splitext(table_name)[0]
+    return (
+        supabase.table("user_datasets")
+        .insert({
+            "table_name": table_name
+        })
+        .execute()
+    )
