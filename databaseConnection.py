f7eb4521ac49bb1dd345c49e526a7038dd7dff2f
helen <helenmarys1023@gmail.com>
2026-03-25 19:57:46 +0530

    the table can be uploaded but problems with returning datatype of column

--- databaseConnection.py
+++ databaseConnection.py
@@ -6,16 +6,50 @@ import psycopg2
 import pandas as pd
 
 load_dotenv()
-db_url = os.environ.get("DB_URL")
+
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
-def insert_user_dataset(table_name):
+def insert_user_dataset(filename):
+    df = pd.read_csv('uploads/'+filename)
+
+    dtype_mapping = {
+        "object": "TEXT",
+        "int64": "BIGINT",
+        "int32": "INTEGER",
+        "float64": "DOUBLE PRECISION",
+        "float32": "REAL",
+        "bool": "BOOLEAN",
+        "datetime64[ns]": "TIMESTAMP",
+        "timedelta[ns]": "INTERVAL"
+    }
+
+    column_defs = []
+
+    for column in df.columns:
+        pandas_dtype = df[column].dtype
+        postgres_dtype = dtype_mapping.get(pandas_dtype, "TEXT")
+        column_defs.append(f'"{column}" {postgres_dtype}')
     
-    table_name = os.path.splitext(table_name)[0]
-    
+    columns_sql = ", ".join(column_defs)
+
+    table_name = os.path.splitext(filename)[0]
+
+    conn = psycopg2.connect(os.environ.get("DB_URL"))
+    cur = conn.cursor()
+
+    cur.execute(f"""
+        CREATE TABLE IF NOT EXISTS "{table_name}" (
+            {columns_sql}
+        )
+    """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
     return (
         supabase.table("user_datasets")
         .insert({
@@ -24,21 +58,8 @@ def insert_user_dataset(table_name):
         .execute()
     )
 
-    
-conn = psycopg2.connect(db_url)
-cur = conn.cursor()
 
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS {table_name} (
-        id SERIAL PRIMARY KEY,
-        name TEXT,
-        city TEXT
-    )
-""")
 
-conn.commit()
-cur.close()
-conn.close()
 
 
 
